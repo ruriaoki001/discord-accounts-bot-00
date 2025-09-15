f4ffe817353f39b1d7923498bcf07b1f3cc17ca4
ruriaoki001 <ruriaoki001@gmail.com>
2025-09-15 20:00:28 +0600

    Update db.js

--- db.js
+++ db.js
@@ -1,18 +1,11 @@
 const Database = require("better-sqlite3");
-const fs = require("fs");
-const path = require("path");
 
-const dbPath = "./data/users.db";
+// Store DB in root
+const db = new Database("./users.db");
 
-// Ensure data folder exists
-fs.mkdirSync(path.dirname(dbPath), { recursive: true });
-
-const db = new Database(dbPath);
-
-// Create users table
 db.prepare(`
   CREATE TABLE IF NOT EXISTS users (
-    user_id TEXT PRIMARY KEY,
+    id TEXT PRIMARY KEY,
     access_token TEXT,
     refresh_token TEXT,
     expires_at INTEGER
